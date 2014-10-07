cce91d7aaaf882a779a463804cc18ee41e571d82
Daniel <danieljfa@gmail.com>
2014-10-07 01:25:05 -0300

    Método novo produto

--- api/classes/produto.php
+++ api/classes/produto.php
@@ -13,4 +13,33 @@ class Produto{
 	{
 		return $produto->nome;
 	}
+
+	public function post_novo($produto){
+
+		//Verifica se nome do produto foi preenchido
+		if (empty($produto->nome))
+			throw new Exception("Nome do produto não pode ser vazio.");			
+
+		//Verifica se o produto já existe
+		$sqlVerificaProduto = "SELECT id,nome FROM Produtos WHERE (nome=:nome)";
+		$stmtVerificaProduto = DB::prepare($sqlVerificaProduto);
+		$stmtVerificaProduto->bindValue("nome", $produto->nome);
+		$stmtVerificaProduto->execute();
+		$produtoEncontrado = $stmtVerificaProduto->fetch();
+		if ($produtoEncontrado)
+            			throw new Exception("Este produto '{$produtoEncontrado->nome}' já existe.");
+
+                	//Inserir produto
+		$sql= "INSERT INTO Produtos (nome) VALUES (:nome)";
+		$stmt = DB::prepare($sql);
+		$stmt->bindParam("nome", $produto->nome);
+		$stmt->execute();
+
+		 $produto->id = DB::lastInsertId();
+        		return $produto;
+
+	}
+
+
+
 }
