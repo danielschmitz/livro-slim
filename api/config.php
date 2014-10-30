4581b427cd10b6763b46ce90934ce058ed6b521e
Daniel Schmitz <danieljfa@gmail.com>
2014-10-30 21:24:11 +0000

    Alteracao no config para o cloud9

--- api/config.php
+++ api/config.php
@@ -1,6 +1,14 @@
 <?php
 
+/* Estes valores sao para um banco de dados na propria maquina
 define("DB_HOST","localhost");
 define("DB_NAME","teste");
 define("DB_USER","root");
+define("DB_PASSWORD","");
+*/
+
+//Valores para um banco de dados no cloud9
+define("DB_HOST","0.0.0.0");
+define("DB_NAME","c9");
+define("DB_USER","danielschmitz");
 define("DB_PASSWORD","");
